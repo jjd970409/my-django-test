ef242ee917cefb5fa9b23648ab2fbfa7cc7cd8b4
jjd970409 <jjd970409@gmail.com>
2025-05-13 20:28:27 +0900

    test

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views  # 로그인/로그아웃 뷰 임포트
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
